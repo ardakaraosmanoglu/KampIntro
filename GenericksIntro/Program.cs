c1b497903464a17b5fd696f2a38ccf28e75da795
ardakaraosmanoglu <ardakaraosmanoglu7@gmail.com>
2021-01-23 19:47:43 +0200

    20 Ocak Ders Sonu

---
+++ GenericksIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericksIntro
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Arda");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
