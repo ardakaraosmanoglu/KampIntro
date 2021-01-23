012ddd4316476d14ece1def876efdb67d64dc9c8
ardakaraosmanoglu <ardakaraosmanoglu7@gmail.com>
2021-01-23 22:55:53 +0200

    23 Ocak Cmt Ders Sonu

---
+++ OOP3/IhtiyacKrediManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace OOP3
+{
+    class IhtiyacKrediManager : IKrediManager
+    {
+        public void BirseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("İhtiyac kredisi ödeme planı hesaplandı.");
+        }
+    }
+}
---
+++ OOP3/KonutKrediManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace OOP3
+{
+    class KonutKrediManager : IKrediManager
+    {
+        public void BirseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("Konut kredisi odeme zamani hesaplandi. ");
+        }
+    }
+}
